0bafc4d94456726b85a5bd493cb34d1264272fba
lematusiak <leematusiak@gmail.com>
2023-10-31 18:44:51 +0100

    Hermetyzacja Metod

---
+++ Zula/Zula/Program.cs
@@ -0,0 +1,32 @@
+﻿// METODY HERMETYZACJA (2023.11.01)
+// Chcemy całą historię punktów przechowywać wewnątrz
+//
+
+
+User user1 = new User("Adam", "123");
+User user2 = new User("Zula", "123");
+User user3 = new User("Pola", "123");
+User user4 = new User("Olek", "123");
+
+user1.AddScore(5);
+
+class User
+{
+    private List<int> score = new List<int>();  
+
+    public User(string login, string password)
+    {
+        this.Login = login;
+        this.Password = password;
+    }
+    public string Login { get; private set; }
+    public string Password { get; private set; }
+
+    public void AddScore(int number)
+    {
+        this.score.Add(number);
+    }
+
+
+
+}
